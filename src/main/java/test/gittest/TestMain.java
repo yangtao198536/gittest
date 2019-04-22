4ef52da733902e6897c78d12e2cbf44c05044394
yangtao198536 <yangtao19850306@163.com>
2019-04-22 19:58:21 +0800

    主变动

--- src/main/java/test/gittest/TestMain.java
+++ src/main/java/test/gittest/TestMain.java
@@ -4,6 +4,6 @@ package test.gittest;
 public class TestMain extends App {
 
 	public static void main(String[] args) {
-		System.out.println( "Hello TestMain!" );
+		System.out.println( "Hello TestMain!--------" );
 	}
 }
